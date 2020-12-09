9fec53b812a4549676ded8a75553708d90dffd23
panupriya <71261615+panupriya@users.noreply.github.com>
2020-12-09 10:48:56 +1000

    MarsDemo

--- MarsFramework/Pages/Chat.cs
+++ MarsFramework/Pages/Chat.cs
@@ -1,4 +1,6 @@
-﻿using SeleniumExtras.PageObjects;
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +16,47 @@ namespace MarsFramework.Pages
             PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
         }
         #region Initialize web elements
+        //Click on Chat tab
+        [FindsBy(How = How.XPath, Using = "//*[@id='account-profile-section']/div/div[1]/div[2]/div/a[1]")]
+        private IWebElement clickChat { get; set; }
 
+        //Select name
+        [FindsBy(How = How.XPath, Using = "//*[@id='chatList']/div[4]/div[2]/div[1]")]
+        private IWebElement EnterChatSel { get; set; }
+
+        //Select chat box to enter data
+        [FindsBy(How = How.XPath, Using = "//*[@id='chatTextBox']")]
+        private IWebElement EnterChat { get; set; }
+
+        //Click on Send tab
+        [FindsBy(How = How.XPath, Using = "//*[@id='btnSend']")]
+        private IWebElement clickSend { get; set; }
         #endregion
+
+        #region Chat
         internal void Chats()
         {
-           
+            ////Populate the Excel Sheet
+            //GlobalDefinitions.ExcelLib.PopulateInCollection(Base.ExcelPath, "Chat");
+
+            //Click on Chat tab
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='account-profile-section']/div/div[1]/div[2]/div/a[1]", 10000);
+            clickChat.Click();
+
+            //Select name
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='chatList']/div[4]/div[2]/div[1]", 10000);
+            EnterChatSel.Click();
+
+            //Select chat box to enter data
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='chatTextBox']", 10000);
+            EnterChat.Click();
+            //EnterChat.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Message"));
+            EnterChat.SendKeys("Hi");
+
+            //Click on Send tab
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='btnSend']", 10000);
+            clickSend.Click();
         }
+        #endregion
     }
 }
--- MarsFramework/Pages/Notification.cs
+++ MarsFramework/Pages/Notification.cs
@@ -1,9 +1,12 @@
 ﻿using SeleniumExtras.PageObjects;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MarsFramework.Global;
+using System.Threading;
 
 namespace MarsFramework.Pages
 {
@@ -16,10 +19,70 @@ namespace MarsFramework.Pages
         }
         #region Initialize web elements
 
+        //Click on Notification tab
+        [FindsBy(How = How.XPath, Using = "//*[@id='account-profile-section']/div/div[1]/div[2]/div/div")]
+        private IWebElement ClickNotification { get; set; }
+
+        //Click on See all
+        [FindsBy(How = How.LinkText, Using = "See All...")]
+        private IWebElement ClickSeeAll { get; set; }
+
+        //Click on select all
+        
+        [FindsBy(How = How.XPath, Using = "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[1]/i")]
+        private IWebElement SelectAll { get; set; }
+
+
+        //UnSelect All
+        [FindsBy(How = How.XPath, Using = "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[2]")]
+        private IWebElement UnSelectAll { get; set; }
+
+        //Select one
+        [FindsBy(How = How.XPath, Using = "//*[@id='notification-section']/div[2]/div/div/div[3]/div[2]/span/span/div/div[1]/div/div/div[3]/input")]
+        private IWebElement SelectOne { get; set; }
+
+        //Mark Selction as read
+        [FindsBy(How = How.XPath, Using = "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[4]")]
+        private IWebElement MarkSelection { get; set; }
+
+        //Delete
+        [FindsBy(How = How.XPath, Using = "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[3]/i")]
+        private IWebElement Delete { get; set; }
         #endregion
+
+        #region Notification
         internal void Notifications()
         {
-            
+            //Click on Notification tab
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='account-profile-section']/div/div[1]/div[2]/div/div", 10000);
+            ClickNotification.Click();
+
+            //Click on See all
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "LinkText", "See All...", 10000);
+            ClickSeeAll.Click();
+
+            //Click on select all
+           
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[1]/i", 10000);
+            SelectAll.Click();
+            Thread.Sleep(1000);
+
+            //UnSelect All
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[2]", 10000);
+            UnSelectAll.Click();
+
+            //Select one
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='notification-section']/div[2]/div/div/div[3]/div[2]/span/span/div/div[1]/div/div/div[3]/input", 10000);
+            SelectOne.Click();
+
+            //Mark Selction as read
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[4]", 10000);
+            MarkSelection.Click();
+
+            //Delete Notification
+            GlobalDefinitions.WaitForElementVisibility(GlobalDefinitions.driver, "XPath", "//*[@id='notification-section']/div[2]/div/div/div[3]/div[1]/div[3]/i", 10000);
+            Delete.Click();
         }
+        #endregion
     }
 }
